124762591b650239dbb52715e7412b10c35c45f3
Christian <christian@haschek.at>
2018-01-03 21:46:40 +0100

    init

---
+++ backend.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+  cactus lamp controller
+
+LED IDs:
+ [1] [3] [7]
+ [2] [4] [8]
+  \  [5]  /
+     [6]
+
+
+    Now add the GPIO pin IDs to the corresponding LED IDs below
+    like this:
+    
+    $pins[ LED ID ] = GPIO PIN
+*/
+
+$pins[1] = 2;
+$pins[2] = 3;
+$pins[3] = 14;
+$pins[4] = 4;
+$pins[5] = 15;
+$pins[6] = 23;
+$pins[7] = 17;
+$pins[8] = 18;
+
+if($_GET['getstatus'])
+{
+    foreach($pins as $lamp => $pin)
+    {
+        $lampstatus[$lamp] = rand(0,1);//;intval(trim(shell_exec("sudo /usr/bin/gpio -g read $pin"))); //TODO!!!!!!!!!!!
+    }
+
+    exit(json_encode($lampstatus));
+}
+
+$lamp = $_REQUEST['lamp'];
+$status = $_REQUEST['status'];
+$port = $pins[$lamp];
+
+//if we got some crazy data, lets not do anything
+if(!is_numeric($lamp) || !$port || !is_numeric($status) || !in_array($lamp,$pins) || ($status>1 && $status<0))
+  exit('Nope');
+
+exit('OK'); //TODO!!!!!!!!!!!
+
+//execute the command
+@shell_exec("sudo /usr/bin/gpio -g write $port $status");
+echo "OK";
---
+++ js/cactus.js
@@ -0,0 +1,37 @@
+var timer;
+var checkinterval = 10;//sek
+
+jQuery.ajaxSetup({ cache: false });
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function loadData()
+{
+    $.get( "backend.php?getstatus=1", function( data ) {
+        $.each(data, function(led, status) {
+            $('#led'+led).attr('checked', (status==1?true:false));
+        })
+    }, "json" );
+}
+
+$(document).ready(function() {
+
+    $('.led').change(function() {
+        var status = this.checked;
+        var led = $(this).attr('led');
+        console.log(led+" changed to "+status);
+        $.get( "backend.php?lamp="+led+"&status="+status, function( data ){
+            $.each(data, function(led, status) {
+                $('#led'+led).attr('checked', (status==1?true:false));
+            })
+        }, "json" );
+    });
+});
+
+timer = setInterval(function(){
+    loadData();
+},checkinterval*1000);
